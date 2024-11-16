4c218b55d553d7c2442593181b0d8ff30abd5799
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2024-11-16 23:27:40 +0000

    created repeated image background for the chat page

--- src/pages/user/ChatPage.tsx
+++ src/pages/user/ChatPage.tsx
@@ -1,9 +1,18 @@
-
+import decor from "../../assets/decor.png";
 
 const ChatPage = () => {
   return (
-    <div>ChatPage</div>
-  )
-}
+    <main
+      className="w-full h-full bg-repeat"
+      style={{ backgroundImage: `url('${decor}')`, backgroundSize: "50px" }}
+    >
+      <div className="w-full h-full backdrop-blur-sm p-10">
+        <section className="">
+          <p>sometin</p>
+        </section>
+      </div>
+    </main>
+  );
+};
 
-export default ChatPage
+export default ChatPage;
