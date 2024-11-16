a68448f156af1b53255ef20c49473684f5aba4cf
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2024-11-16 23:56:44 +0000

    added the write message and other buttons necessary for chatting in chat page

--- src/pages/user/ChatPage.tsx
+++ src/pages/user/ChatPage.tsx
@@ -1,4 +1,6 @@
 import decor from "../../assets/decor.png";
+import { Paperclip, Send, SmilePlus } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
 
 const ChatPage = () => {
   return (
@@ -7,8 +9,26 @@ const ChatPage = () => {
       style={{ backgroundImage: `url('${decor}')`, backgroundSize: "50px" }}
     >
       <div className="w-full h-full backdrop-blur-sm p-10">
-        <section className="">
+        <section className="h-[90%]">
           <p>sometin</p>
+        </section>
+
+        <section className="flex justify-center items-center space-x-3">
+          <div className="p-2 hover:bg-sky-900 hover:cursor-pointer transition-all duration-300 ease-in rounded-full">
+            <SmilePlus />
+          </div>
+
+          <div className="p-2 hover:bg-sky-900 hover:cursor-pointer transition-all duration-300 ease-in rounded-full">
+            <Paperclip />
+          </div>
+          <Textarea
+            className="w-11/12 bg-sky-800 shadow-inner shadow-sky-900"
+            placeholder="Write something here"
+          />
+
+          <div className="p-2 hover:bg-sky-900 hover:cursor-pointer transition-all duration-300 ease-in rounded-full">
+            <Send />
+          </div>
         </section>
       </div>
     </main>
