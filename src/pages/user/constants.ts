f330433e5b85171e8c00cb91bdc235c82ac133b2
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-01-10 23:17:48 +0000

    refactored Chat page and created a new user container component for users in chat page

---
+++ src/pages/user/constants.ts
@@ -0,0 +1,26 @@
+export const users = [
+  {
+    name: "josh",
+    newMessage: "",
+    date: "",
+    notifications: "",
+  },
+  {
+    name: "angela",
+    newMessage: "",
+    date: "",
+    notifications: "",
+  },
+  {
+    name: "bran",
+    newMessage: "",
+    date: "",
+    notifications: "",
+  },
+  {
+    name: "dennis",
+    newMessage: "",
+    date: "",
+    notifications: "",
+  },
+];
