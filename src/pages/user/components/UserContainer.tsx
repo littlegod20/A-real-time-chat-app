cdfd5f2c08919c83259fc265b0e45491067ab46a
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-01-11 17:42:41 +0000

    fixed the user list container problem

--- src/pages/user/components/UserContainer.tsx
+++ src/pages/user/components/UserContainer.tsx
@@ -16,24 +16,29 @@ const UserContainer = () => {
     <>
       {userList &&
         userList.map((item, index) => (
-          <div className="flex gap-2 w-full" key={index}>
-            {item.avatar && (
-              <img
-                src={item.avatar}
-                alt="avatar name"
-                key={index}
-              />
-            )}
-            <div className="flex flex-col justify-between w-4/5">
-              <div className="flex justify-between">
-                <header className="font-semibold text-base">{item.userName}</header>
-                <div>date</div>
+          <div className="w-full" key={index}>
+            <div className="flex  bg-sky-950 gap-2 p-2 sm:max-w-[300px] lg:min-w-[250px] lg:max-w-[500px] rounded-md">
+              <div className="flex">
+                {item.avatar && (
+                  <img src={item.avatar} alt="avatar name" key={index} />
+                )}
               </div>
-              <p className="font-normal text-sm truncate">
-                user message sifowe owfiowo dwfoeiw foweifowdi oweifwofj
-                ewoiefjwoeijfwodijwoifjweiofjwoij woiefjwoi ejfowifjwoijwofi
-                wjfoiwjfweoifjwe
-              </p>
+              <div className="flex flex-col justify-between w-4/5">
+                <div className="flex justify-between">
+                  <header className="font-semibold text-base">
+                    {item.userName}
+                  </header>
+                  <div>date</div>
+                </div>
+                <div className="flex-1">
+                  <p className="font-normal text-sm truncate">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Pariatur amet eum ducimus animi maiores quo impedit
+                    voluptate vero dolore corrupti? Illo veniam quis in eos
+                    porro deleniti cum est deserunt!
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         ))}
